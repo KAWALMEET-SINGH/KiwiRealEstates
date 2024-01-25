51340a42f78f995384e4aa3021651238aee5fb82
Kawalmeet Singh <kmssr782002@gmail.com>
2024-01-25 14:47:28 +0530

    Error Handler & middleware

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signUp = async(req,res) =>{
+export const signUp = async(req,res,next) =>{
     const {username,email,password} = req.body;
     const passwordLock =  bcryptjs.hashSync(password,12);
     const newUser = new User({username,email,password:passwordLock});
@@ -9,7 +9,7 @@ export const signUp = async(req,res) =>{
          await newUser.save();
     res.status(201).json({"message" : "user created"});
 }catch(e){
-res.status(500).json(e.message);
+    next(e);
 }
     }
    
--- server/index.js
+++ server/index.js
@@ -23,6 +23,16 @@ app.get('/',(req,res) =>{
     })
 })
 
+app.use((err,req,res,next)=>{
+    const status = err.statusCode || 500 ;
+    const message = err.message || "Internal Server Error";
+    return res.status(status).json({
+        success : false,
+        statusCode:status,
+        message
+    })
+})
+
 app.listen(3000,() =>{
 console.log("server started");
 }
