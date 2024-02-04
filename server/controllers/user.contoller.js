5da581bc683357195fd9d57d980e60137648d367
Kawalmeet Singh <kmssr782002@gmail.com>
2024-02-04 14:28:29 +0530

    User Update Api Created

--- server/controllers/user.contoller.js
+++ server/controllers/user.contoller.js
@@ -1,5 +1,34 @@
-export const test = (req,res) =>{
-    res.json({
-        massage:"Nuru Massage"
-    })
-}
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const test = (req, res) => {
+  res.json({
+    massage: "Message",
+  });
+};
+export const updateUser = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(errorHandler(401, "Invalid User Id"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 12);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.parama.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(201).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
