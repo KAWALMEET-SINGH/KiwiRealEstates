b59bffbd1abf3188ca3f83fed3bfad84b99646ea
Kawalmeet Singh <kmssr782002@gmail.com>
2024-02-11 19:15:53 +0530

    about page added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Kiwi Estates</h1>
+      <p className='mb-4 text-slate-700'>At Kiwi Estates, we pride ourselves on being a premier real estate agency dedicated to delivering exceptional service and unparalleled expertise in the property market. With a focus on luxury living and personalized solutions, we strive to exceed the expectations of our clients at every turn.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is simple: to help individuals and families find their dream homes, and to assist sellers in maximizing the value of their properties. Backed by a team of experienced agents with an intimate knowledge of the local market, we offer guidance, support, and innovative strategies tailored to each client's unique needs and aspirations.
+      </p>
+      <p className='mb-4 text-slate-700'>At Kiwi Estates, we believe that real estate is more than just buying and selling properties—it's about creating connections, building communities, and turning dreams into reality. Whether you're searching for the perfect penthouse or looking to list your luxury property, trust Kiwi Estates to be your partner every step of the way.</p>
+    </div>
   )
 }
 
